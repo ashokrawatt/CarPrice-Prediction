eebbf5b87814b85d34175821f0818025d5e86ee6
Ashok singh <rwtashok2000@gmail.com>
2023-04-29 08:52:06 +0530

    changes in application.py

--- application.py
+++ application.py
@@ -4,10 +4,9 @@ import pickle
 import pandas as pd
 import numpy as np
 
+
 app=Flask(__name__)
-cors=CORS(app)
-model=pickle.load(open('LinearRegressionModel.pkl','rb'))
-car=pd.read_csv('Cleaned_Car_data.csv')
+car=pd.read_csv("notebooks/Cleaned_Car_data.csv")
 
 @app.route('/',methods=['GET','POST'])
 def index():
@@ -20,24 +19,5 @@ def index():
     return render_template('index.html',companies=companies, car_models=car_models, years=year,fuel_types=fuel_type)
 
 
-@app.route('/predict',methods=['POST'])
-@cross_origin()
-def predict():
-
-    company=request.form.get('company')
-
-    car_model=request.form.get('car_models')
-    year=request.form.get('year')
-    fuel_type=request.form.get('fuel_type')
-    driven=request.form.get('kilo_driven')
-
-    prediction=model.predict(pd.DataFrame(columns=['name', 'company', 'year', 'kms_driven', 'fuel_type'],
-                              data=np.array([car_model,company,year,driven,fuel_type]).reshape(1, 5)))
-    print(prediction)
-
-    return str(np.round(prediction[0],2))
-
-
-
 if __name__=='__main__':
-    app.run()
+    app.run(debug=True)
